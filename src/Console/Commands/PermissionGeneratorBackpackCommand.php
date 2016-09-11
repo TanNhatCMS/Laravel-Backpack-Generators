a2efb43046f160454834b66c9e2ac607fff5529c
OwenMelbz <owenmelbz@gmail.com>
2016-09-11 20:38:38 +0100

    updated formatting

--- src/Console/Commands/PermissionGeneratorBackpackCommand.php
+++ src/Console/Commands/PermissionGeneratorBackpackCommand.php
@@ -2,39 +2,38 @@
 
 namespace Backpack\Generators\Console\Commands;
 
-use Artisan;
 use Illuminate\Console\Command;
 use Spatie\Permission\Models\Permission;
 
 class PermissionGeneratorBackpackCommand extends Command
 {
     /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+    * The name and signature of the console command.
+    *
+    * @var string
+    */
      protected $signature = 'backpack:permissions {route : Name of the route e.g admin/teams} {--P|permission=* : A permission you wish to add, can accept multiple instances of -P|permission}';
 
     /**
-     * The console command description.
-     *
-     * @var string
-     */
+    * The console command description.
+    *
+    * @var string
+    */
     protected $description = 'Will generate permissions for routes and save to the database';
 
     /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
+    * Execute the console command.
+    *
+    * @return mixed
+    */
     public function handle()
     {
-        $routeName         = $this->argument('route');
-        $permissions       = ['list', 'create', 'update', 'reorder', 'delete'];
-        $permissionsAdded  = 0;
+        $routeName = $this->argument('route');
+        $permissions = ['list', 'create', 'update', 'reorder', 'delete'];
+        $permissionsAdded = 0;
         $customPermissions = $this->option('permission');
 
-        if( count($customPermissions) > 0 ){
+        if(count($customPermissions) > 0){
             $permissions = $customPermissions;
         }
 
@@ -42,35 +41,32 @@ class PermissionGeneratorBackpackCommand extends Command
 
         foreach($permissions as $permission){
 
-            $permissionName = trim($routeName, '/') . '/' . trim($permission, '/');
+            $permissionName = trim($routeName, '/').'/'.trim($permission, '/');
             $permissionName = strtolower($permissionName);
 
             $existingPermission = Permission::where(['name' => $permissionName])->first();
 
-            if( $existingPermission ){
+            if($existingPermission){
 
                 $continueAdding = $this->confirm("$permissionName already exists, do you want to carry on? [y|N]");
 
-                if( $continueAdding ){
+                if($continueAdding){
                     $bar->setMessage("Skipping $permissionName");
                     $bar->advance();
                     continue;
-                }
-                else {
+                } else {
                     $bar->finish();
                     return $this->error("\nCancelled adding any more permissions.");
                 }
-            }
-            else {
+            } else {
                 $newPermission = Permission::create(['name' => $permissionName]);
                 $newPermission->save();
 
-                if( $newPermission->id ){
+                if($newPermission->id){
                     $permissionsAdded++;
                     $bar->setMessage("$newPermission->name added with ID: $newPermission->id");
                     $bar->advance();
-                }
-                else {
+                } else {
                     $bar->setMessage("$permissionName could not be created.");
                     $bar->advance();
                 }
